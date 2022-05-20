d43e81c9eee67fc565eae4e88c24092c2394b3a0
Aymen <aymensbc@gmail.com>
2022-05-20 15:40:23 +0500

    Error handling and express async handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,37 @@
+//---WHY USE EXPRESS ASYNC HANDLER : When we use Mongoose to connect with the database , these functions return a promise
+//So we have to add async await in a try catch block. In order to avoid the try catch block, we can use the express-async-handler: npm i express-async-handler
+const asyncHandler = require("express-async-handler");
 // @desc    Get goals
 // @route   GET api/goals
 // @access  Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "GET GOALS" });
-};
+});
 
 // @desc    Set goals
 // @route   POST api/goals
 // @access  Private
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
+  if (!req.body.name) {
+    res.status(400);
+    throw new Error("Please enter text msg");
+  }
   res.status(200).json({ message: "CREATE GOALS" });
-};
+});
 
 // @desc    Update goals
 // @route   PUT api/goals
 // @access  Private
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `UPDATE GOAL  ${req.params.id}` });
-};
+});
 
 // @desc    delete goals
 // @route   DELETE api/goals
 // @access  Private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `DELETE GOALS ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,15 @@
+//this error handle is created inorder  to override the default express error handler
+//setGoals controller uses this error handler.
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500; //500 is a server error
+
+  res.json({
+    message: err.message,
+    stack: (process.env.NODE_ENV = "production" ? null : err.stack), //if in production then dont show error stack
+  });
+};
+
+module.exports = {
+  errorHandler,
+};
