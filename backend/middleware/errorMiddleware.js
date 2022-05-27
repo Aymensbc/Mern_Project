ac2a78e21a70fb69775fa8af5f4f550e79eaae0e
Aymen <aymensbc@gmail.com>
2022-05-27 09:49:20 +0500

    Protect goals in goal route

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -6,7 +6,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.json({
     message: err.message,
-    stack: (process.env.NODE_ENV = "production" ? null : err.stack), //if in production then dont show error stack
+    stack: process.env.NODE_ENV === "production" ? null : err.stack, //if in production then dont show error stack
   });
 };
 
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,13 +7,15 @@ const {
   updateGoals,
 } = require("../controllers/goalController");
 
+const { protect } = require("../middleware/authMiddleware");
+
 // router.get("/", getGoals);
 // router.post("/", setGoals);
 //Instead of above we can write in the following way :
-router.route("/").get(getGoals).post(setGoals);
+router.route("/").get(protect, getGoals).post(protect, setGoals);
 
 // router.put("/:id", updateGoals);
 // router.delete("/:id", deleteGoals);
-router.route("/:id").put(updateGoals).delete(deleteGoals);
+router.route("/:id").put(protect, updateGoals).delete(protect, deleteGoals);
 
 module.exports = router;
