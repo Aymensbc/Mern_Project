4d2978b21e846208e1acfc3657839511bf2cc7e6
Aymen <aymensbc@gmail.com>
2022-05-31 17:22:45 +0500

    user registration and axios

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,19 @@
+//this will take care of http requets
+import axios from "axios";
+const API_URL = "api/users";
+
+const register = async (userData) => {
+  const response = await axios.post(API_URL, userData);
+
+  //axios puts response into object called data by default. So we check our local storage . We use JSON.stringify becasue we need to put strings in the local storage. In localstorage.setItem , first argument is the name and the other is the value ( key value pair) response.data includes our token.
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
+const authService = {
+  register,
+};
+
+export default authService;
